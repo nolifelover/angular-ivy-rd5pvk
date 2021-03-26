e0d379a082a60bbe8b9aebc2ee94e1b4e562c941
nolifelover <nolifelover@gmail.com>
2021-03-26 23:19:47 +0700

    2 files modified, 4 added

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,13 +1,21 @@
-import { NgModule } from '@angular/core';
-import { BrowserModule } from '@angular/platform-browser';
-import { FormsModule } from '@angular/forms';
+import { NgModule } from "@angular/core";
+import { BrowserModule } from "@angular/platform-browser";
+import { FormsModule } from "@angular/forms";
 
-import { AppComponent } from './app.component';
-import { HelloComponent } from './hello.component';
+import { AppComponent } from "./app.component";
+import { HelloComponent } from "./hello.component";
+import { OneComponent } from "./one/one.component";
+import { TwoComponent } from "./two/two.component";
+import { RouterModule, Routes } from "@angular/router";
+
+const routes: Routes = [
+  { path: "one", component: OneComponent },
+  { path: "two", component: TwoComponent }
+];
 
 @NgModule({
-  imports:      [ BrowserModule, FormsModule ],
-  declarations: [ AppComponent, HelloComponent ],
-  bootstrap:    [ AppComponent ]
+  imports: [BrowserModule, FormsModule, RouterModule],
+  declarations: [AppComponent, HelloComponent, OneComponent, TwoComponent],
+  bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {}
